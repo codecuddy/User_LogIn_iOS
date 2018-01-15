c60ff4bac182e410454d0ce2dc3b95744ece62cb
Charlie <charliecuddy4@gmail.com>
2018-01-15 15:19:08 -0600

    Initial Commit

---
+++ User_LogIn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  User_LogIn
+//
+//  Created by Charlie Cuddy on 1/15/18.
+//  Copyright © 2018 Charlie Cuddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
