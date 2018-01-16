eba44c08ba1dfb31bf7c0923bdb713a0d2cfc0dd
Charlie <charliecuddy4@gmail.com>
2018-01-15 21:10:36 -0600

    begin set up for user log in demo

--- User_LogIn/ViewController.swift
+++ User_LogIn/ViewController.swift
@@ -7,12 +7,53 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func logIn(_ sender: Any) {
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            
+            let results = try context.fetch(request)
+            
+            for result in results as! [NSManagedObject] {
+                
+                if let username = result.value(forKey: "name") as? String {
+                    
+                    textField.alpha = 0
+                    logIn.alpha = 0
+                    label.alpha = 1
+                    
+                }
+                
+            }
+            
+        } catch {
+            
+            print("Request failed")
+            
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
